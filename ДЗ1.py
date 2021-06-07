ea22e71da6f72ff1356121ca4ffccaf9a172b422
HexAnna <85127369+HexAnna@users.noreply.github.com>
2021-06-07 17:23:39 +0400

    Add files via upload

---
+++ ДЗ1.py
@@ -0,0 +1,18 @@
+# Булдакова Анна Евгеньевна ДЗ 1
+#1.1
+a = int (input('a:'))
+b = int (input('b:'))
+c = int (input('c:'))
+print (a + b + c)
+#1.2
+h = int (input('hight:'))
+b = int (input('base:'))
+print (float (0.5 * b * h))
+#1.3
+n = int (input('Школьников:'))
+k = int (input ('Яблок:'))
+print (k//n)
+print (k%n)
+
+
+
